5c18c264229af172ecf2858839506481042fe47e
JeongJeongIn <devforin@gmail.com>
2025-02-20 00:17:33 +0900

    feat: make Todo

---
+++ src/main/java/com/mooo/devforin/appservice/controller/todo/TodoController.java
@@ -0,0 +1,13 @@
+package com.mooo.devforin.appservice.controller.todo;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/todo")
+public class TodoController {
+}
