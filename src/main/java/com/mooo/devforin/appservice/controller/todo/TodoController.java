a7830c7d9d4e9eb3de78bc95b3b06d0e9e09dd46
JeongJeongIn <devforin@gmail.com>
2025-02-22 23:37:13 +0900

    feat: 인증 실패 에러 추가

--- src/main/java/com/mooo/devforin/appservice/controller/todo/TodoController.java
+++ src/main/java/com/mooo/devforin/appservice/controller/todo/TodoController.java
@@ -1,7 +1,10 @@
 package com.mooo.devforin.appservice.controller.todo;
 
+import com.mooo.devforin.appservice.config.global.ResponseDTO;
+import com.mooo.devforin.appservice.config.global.ResponseUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/todo")
 public class TodoController {
+
+    @GetMapping("/get-list")
+    public ResponseDTO<Object> getList(){
+        log.info("get List Controller.");
+      return ResponseUtil.SUCCESS(200, "Get List !", null);
+    }
 }
